b2c5f297303f3c96a68b49c9b9ed1ab9d31077cd
NguyenVohan <hovannguyenvl@gmail.com>
2025-09-30 22:56:53 +0700

    feat: home and auth page

--- src/app/routes/MainRoutes.tsx
+++ src/app/routes/MainRoutes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../layouts/Header/Header';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
+import AuthPage from '../pages/Auth/auth';
 
 const MainRoutes: React.FC = () => {
     return (
@@ -11,6 +12,7 @@ const MainRoutes: React.FC = () => {
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
+                <Route path='/auth' element={<AuthPage />} />
             </Routes>
         </BrowserRouter>
     );
