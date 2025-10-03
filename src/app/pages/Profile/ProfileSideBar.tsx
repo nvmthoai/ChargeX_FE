de753c10a0997f58f37efeee37fc80b7048d00ec
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-10-04 00:23:54 +0700

    Add profile

---
+++ src/app/pages/Profile/ProfileSideBar.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./ProfileSideBar.css";
+
+export default function ProfileSideBar() {
+    console.log("ProfileSideBar console");
+
+    const [Option, setOption] = useState("");
+    const location = useLocation();
+
+    useEffect(() => {
+        setOption(location.pathname);
+    }, [location])
+
+    const ListOption = [
+        { name: "Profile Details", link: "detail" },
+        { name: "Security Settings", link: "security" },
+        { name: "Notification Preferences", link: "notification" },
+        { name: "Linked Accounts", link: "linked-account" },
+    ];
+
+    return (
+        <div className="profilesidebar-container">
+            {ListOption.map((option) => (
+                <Link
+                    to={option.link}
+                    key={option.link}
+                    onClick={() => setOption(option.link)}
+                >
+                    <div className={`option ${Option?.includes(option.link) ? "active" : ""}`}>
+                        <div className="option-name">{option.name}</div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
+}
--- src/app/routes/MainRoutes.tsx
+++ src/app/routes/MainRoutes.tsx
@@ -1,15 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from '../layouts/Header/Header';
-import About from '../pages/About/About';
-import Home from '../pages/Home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "../layouts/Header/Header";
+import About from "../pages/About/About";
+import Home from "../pages/Home/Home";
+import Profile from "../pages/Profile/Profile";
+import ProfileDetail from "../pages/Profile/ProfileDetail/ProfileDetail";
+import ProfileSecurity from "../pages/Profile/ProfileSecurity/ProfileSecurity";
 
 export default function MainRoutes() {
     return (
         <BrowserRouter>
             <Header />
             <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="profile" element={<Profile />}>
+                    <Route path="detail" element={<ProfileDetail />} />
+                    <Route path="security" element={<ProfileSecurity />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
