9ba8629da2ab96186ecad3bdce783b5ce1472ba7
NguyenVohan <hovannguyenvl@gmail.com>
2025-10-09 01:23:06 +0700

    feat: login with api and protect route

--- src/app/services/AuthService.ts
+++ src/app/services/AuthService.ts
@@ -6,11 +6,10 @@ import { message } from "antd";
 const authSerivce = () => {
   const { callApi, loading, setIsLoading } = useApiService();
 
-
   const register = useCallback(
     async (values: any) => {
       try {
-        const response = await callApi(HTTP_METHOD.POST, '/auth/register', {
+        const response = await callApi(HTTP_METHOD.POST, "/auth/register", {
           ...values,
         });
         return response;
@@ -21,9 +20,22 @@ const authSerivce = () => {
     [callApi]
   );
 
-
+  const login = useCallback(
+    async (values: any) => {
+      try {
+        const response = await callApi(HTTP_METHOD.POST, "/auth/login", {
+          ...values,
+        });
+        return response;
+      } catch (e: any) {
+        message.error(e?.response?.data);
+      }
+    },
+    [callApi]
+  );
   return {
     register,
+    login,
     loading,
     setIsLoading,
   };
