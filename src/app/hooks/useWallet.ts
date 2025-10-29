4a154022c02ba85c4948b54883b5e058b7236ee4
NguyenVohan <hovannguyenvl@gmail.com>
2025-10-29 09:00:44 +0700

    feat: deposit and get my wallet

---
+++ src/app/hooks/useWallet.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import type { MyWallet } from "../models/wallet.model";
+import walletService from "../services/WalletService";
+
+const useWallet = () => {
+  const [myWallet, setMyWallet] = useState<MyWallet | null>(null);
+  const { getMyWallet, deposit } = walletService();
+  useEffect(() => {
+    fetchMyWallet();
+  }, []);
+
+  const fetchMyWallet = async () => {
+    const response = await getMyWallet();
+    if (response) {
+      setMyWallet(response.data);
+    }
+  };
+
+  const handleDeposit = async (amount: string) => {
+    const response = await deposit(amount);
+    if (response) {
+      fetchMyWallet();
+      window.location.href = response.data.checkoutUrl;
+    }
+  };
+
+  return {
+    myWallet,
+    handleDeposit,
+  };
+};
+
+export default useWallet;
