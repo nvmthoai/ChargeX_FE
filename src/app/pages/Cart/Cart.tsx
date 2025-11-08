e63bbbf4aaad00ead74fe5ea3c7b62fa0d26f985
PanpaKawaii <dngngchitriu2004@gmail.com>
2025-11-09 00:10:40 +0700

    Cart with login user

--- src/app/pages/Cart/Cart.tsx
+++ src/app/pages/Cart/Cart.tsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 import CartItemsList from "./components/CartItemList";
 import OrderSummary from "./components/OrderSummary";
+import { useAuth } from "../../hooks/AuthContext";
 import { fetchData, getQueryString } from "../../../mocks/CallingAPI";
 // import type { CartItem } from "./components/CartItemList";
 // import { initialItems } from "../../../data/initialItems";
 
 
 export default function CartPage() {
+  const { user } = useAuth();
   const [items, setItems] = useState<Record<string, any>[]>([]);
   const [selectedItems, setSelectedItems] = useState<Record<string, any>[]>([]);
+  const [refresh, setRefresh] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
@@ -18,8 +21,10 @@ export default function CartPage() {
       try {
         const ItemsResponse = await fetchData(`/orders${getQueryString({ page: 1, limit: 1000 })}`, token);
         console.log('ItemsResponse', ItemsResponse.data.data);
+        const FilterItems = ItemsResponse?.data?.data?.filter((i: any) => i.buyer?.userId == user?.sub);
 
-        setItems(ItemsResponse.data.data);
+        // setItems(ItemsResponse.data.data);
+        setItems(FilterItems);
       } catch (error) {
         setError('Error');
       } finally {
@@ -28,7 +33,7 @@ export default function CartPage() {
     };
 
     fetchDataAPI();
-  }, []);
+  }, [refresh]);
 
   // const updateQty = (id: number, qty: number) => {
   //   setItems(items.map((i: any) => (i.id === id ? { ...i, qty: Math.max(1, qty) } : i)));
@@ -48,35 +53,45 @@ export default function CartPage() {
     });
   };
 
-  const showSelectedItems = () => {
-    console.log('selectedItems', selectedItems);
-    alert(selectedItems);
-  }
-
   const filteredItems = items.filter(item => selectedItems.includes(item.orderId));
   const subtotal = filteredItems.reduce((sum: any, i: any) => sum + (i.orderShops?.[0]?.orderDetails?.[0]?.price || 0) * i.orderShops?.[0]?.orderDetails?.[0]?.quantity, 0);
   const shipping = filteredItems.reduce((sum: any, i: any) => sum + parseInt(i.orderShops?.[0]?.shippingFee || 0), 0);
   const tax = subtotal * 0.08;
   const total = subtotal + shipping + tax;
 
-  if (error) return <div><button onClick={() => setError('')}>Error</button></div>
+  if (error) return <div><button onClick={() => setRefresh(p => p + 1)}>Error</button></div>
   if (loading) return <div><button onClick={() => setLoading(false)}>Loading</button></div>
   return (
     <div className="min-h-screen bg-gray-50 p-8">
       <h1 className="text-center text-2xl font-bold mb-8">Your Shopping Cart</h1>
-      <button onClick={() => showSelectedItems()}>SHOW</button>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
-        <section className="lg:col-span-2">
-          <CartItemsList items={items} selectedItems={selectedItems} onRemove={removeItem} onCheck={handleCheckboxChange} />
-        </section>
+        {items?.length > 0 ?
+          <section className="lg:col-span-2">
+            <CartItemsList items={items} selectedItems={selectedItems} onRemove={removeItem} onCheck={handleCheckboxChange} />
+          </section>
+          :
+          <section className="lg:col-span-2">
+            <div style={{
+              padding: '4px 8px',
+              color: '#aaa',
+              background: '#eee',
+              border: '1px solid #e4e4e4ff',
+              borderRadius: '200px',
+              textAlign: 'center',
+              fontStyle: 'italic'
+            }}>
+              No items
+            </div>
+          </section>
+        }
 
         <OrderSummary
           subtotal={subtotal}
           shipping={shipping}
           tax={tax}
           total={total}
-          onCheckout={() => alert("Proceed to Checkout")}
+          onCheckout={() => alert(selectedItems)}
           onContinue={() => alert("Continue Shopping")}
         />
       </div>
