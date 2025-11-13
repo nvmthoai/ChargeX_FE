40771a9697719ca13b75ea9fe8be9b4a52a09b37
Kim Hang <kimhangbt2017@gmail.com>
2025-11-13 12:02:01 +0700

    fix shop and bg color

--- src/app/pages/Shop/component/TransactionHistory.tsx
+++ src/app/pages/Shop/component/TransactionHistory.tsx
@@ -3,7 +3,7 @@ import AllOrder from "../transaction/AllOrder";
 export default function TransactionHistory() {
   return (
     <div>
-      <div className="border border-dashed border-ocean-200/30 rounded-xl p-8 bg-gradient-to-br from-white/95 via-ocean-50/20 to-energy-50/20 text-dark-400 text-center shadow-sm">
+      <div className="border border-dashed border-gray-300 rounded-xl p-8 bg-white text-gray-500 text-center shadow-sm">
               <AllOrder/>
             </div>
     </div>
